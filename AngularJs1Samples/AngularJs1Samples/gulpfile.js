226078707959647f1a6d2c9b43ffa405c1762645
SOLARIS\Anton <anton.baula@gmail.com>
2016-09-03 21:44:01 +0300

    Настроил среду разработки.

---
+++ AngularJs1Samples/AngularJs1Samples/gulpfile.js
@@ -0,0 +1,98 @@
+﻿/// <binding BeforeBuild='clean:wwwroot, copyAppJsScripts, copyAppHtmlFiles, copyAppStyles, copyLibJsScripts' Clean='clean:wwwroot' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+var fs = require('fs');
+var path = require('path');
+var dir = require('node-dir');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var del = require('del');
+var header = require("gulp-header");
+var less = require('gulp-less');
+var minifycss = require('gulp-minify-css');
+
+// информация о версии
+var appVersion = fs.readFileSync('version.txt');
+// папка с библиотечными скриптами
+var libsPath = 'bower_components';
+// папка с исходными скриптами
+var scriptsPath = 'scripts';
+// папка куда копируются готовые скрипты
+var outputPath = 'wwwroot';
+
+
+function replaceCoreFolder(dir, newCoreFolder)
+{
+    var dirArr = dir.split(path.sep);
+    dirArr[0] = newCoreFolder;
+    return dirArr.join(path.sep);
+}
+
+gulp.task('copyAppJsScripts', function()
+{
+    dir.subdirs(scriptsPath, function(err, subdirs)
+    {
+        if (err)
+            throw err;
+
+        subdirs.forEach(function(subdir)
+        {
+            gulp.src(path.join(subdir, '/*.js'))
+                .pipe(uglify())
+                .pipe(concat('app.min.js'))
+                .pipe(header(fs.readFileSync('copyright.js.txt'), { version: appVersion }))
+                .pipe(gulp.dest(replaceCoreFolder(subdir, outputPath)));
+        });
+    });
+});
+
+gulp.task('clean:wwwroot', function()
+{
+    return del('wwwroot/**/*');
+});
+
+gulp.task('copyAppHtmlFiles', function()
+{
+    return gulp.src(path.join(scriptsPath, '/**/*.html'))
+        .pipe(header(fs.readFileSync('copyright.html.txt'), { version: appVersion }))
+        .pipe(gulp.dest(outputPath));
+});
+
+gulp.task('copyAppStyles', function()
+{
+    dir.subdirs(scriptsPath, function(err, subdirs)
+    {
+        if (err)
+            throw err;
+
+        subdirs.forEach(function(subdir)
+        {
+            gulp.src(path.join(subdir, '/*.less'))
+                .pipe(less())
+                .pipe(minifycss())
+                .pipe(concat('app.min.css'))
+                .pipe(header(fs.readFileSync('copyright.js.txt'), { version: appVersion }))
+                .pipe(gulp.dest(replaceCoreFolder(subdir, outputPath)));
+        });
+    });
+});
+
+gulp.task('copyLibJsScripts', function()
+{
+    var stream = gulp.src(path.join(libsPath, '/**/*.min.js'));
+
+    dir.subdirs(outputPath, function (err, subdirs)
+    {
+        if (err)
+            throw err;
+
+        subdirs.forEach(function (subdir)
+        {
+            if(path.basename(subdir) === 'js')
+                stream.pipe(gulp.dest(subdir));
+        });
+    });
+});
---
+++ AngularJs1Samples/AngularJs1Samples/scripts/SampleAngularApp/js/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("simpleApp", []);
+    angular.module("simpleApp").controller("ctrl", ["$scope", function ($scope)
+    {
+        $scope.products = ["Milk", "Bread", "Cheese"];
+    }]);
+
+})();
+
+
