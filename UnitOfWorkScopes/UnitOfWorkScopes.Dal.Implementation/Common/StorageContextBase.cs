7dc90b39fce34d32a7e2d897a87b14e11e05acc9
SOLARIS\Anton <anton.baula@gmail.com>
2017-03-10 22:12:06 +0300

    Поправил код инициализации контекста.

--- UnitOfWorkScopes/UnitOfWorkScopes.Dal.Implementation/Common/StorageContextBase.cs
+++ UnitOfWorkScopes/UnitOfWorkScopes.Dal.Implementation/Common/StorageContextBase.cs
@@ -12,12 +12,14 @@ namespace UnitOfWorkScopes.Dal.Implementation.Common
         private readonly ConnectionContextData _connectionContextData;
         private readonly IsolationLevel _isolationLevel;
         private readonly SemaphoreSlim _semaphoreSlim;
+        private bool _initialized;
 
         protected StorageContextBase(DbConnection connection, IsolationLevel isolationLevel)
         {
             _connectionContextData = new ConnectionContextData(connection);
             _isolationLevel = isolationLevel;
             _semaphoreSlim = new SemaphoreSlim(1, 1);
+            _initialized = false;
         }
 
         public void Commit()
@@ -45,23 +47,23 @@ namespace UnitOfWorkScopes.Dal.Implementation.Common
 
         private async Task TryOpenConnectionAsync()
         {
-            if (_connectionContextData.Connection.State != ConnectionState.Closed)
+            if (_initialized)
                 return;
 
             try
             {
                 await _semaphoreSlim.WaitAsync();
 
-                if (_connectionContextData.Connection.State != ConnectionState.Closed)
+                if (_initialized)
                     return;
 
                 await _connectionContextData.Connection.OpenAsync()
                     .ConfigureAwait(false);
 
-                if (_isolationLevel == IsolationLevel.Unspecified)
-                    return;
+                if (_isolationLevel != IsolationLevel.Unspecified)
+                    _connectionContextData.Transaction = _connectionContextData.Connection.BeginTransaction(_isolationLevel);
 
-                _connectionContextData.Transaction = _connectionContextData.Connection.BeginTransaction(_isolationLevel);
+                _initialized = true;
             }
             finally
             {
