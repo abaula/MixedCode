24b02d0a9e1cd0b53f6340da3649654523c42dad
A.Baula <anton.baula@gmail.com>
2023-12-06 16:49:44 +0000

    RunParralelJobBatch.

---
+++ ParralelJobBatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var payloads = Enumerable.Range(1, 1000000).ToArray();
+        RunParralelJobBatch(payloads, Square, 10).GetAwaiter().GetResult();
+    }
+
+    private static Task Square(int value)
+    {
+        Console.WriteLine($"square of {value} = {value * value}");
+
+        return Task.CompletedTask;
+    }
+
+    private static async Task RunParralelJobBatch<TPayload>(TPayload[] payloads, Func<TPayload, Task> job, int parralelJobsLimit)
+    {
+        var jobsSet = payloads
+            .Take(parralelJobsLimit)
+            .Select(_ => job(_))
+            .ToHashSet();
+
+        for (int i = parralelJobsLimit + 1; i < payloads.Length; i++)
+        {
+            var completed = await Task.WhenAny(jobsSet);
+            jobsSet.Remove(completed);
+            jobsSet.Add(job(payloads[i]));
+        }
+    }
+}
